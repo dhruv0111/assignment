af16f2b05f9c205d384ae45957f2945dee0f5c90
dhruv0111 <120778807+dhruv0111@users.noreply.github.com>
2024-08-14 15:56:03 +0530

    Update Workspace.tsx

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Workspace = () => {
   return (
     <div className='flex justify-center items-center gap-2'>
-        <p className='text-[12px]'>Tim's Workspace </p>
+        <p className='text-[12px]'>Dhruv's Workspace </p>
         <span> <ChevronDown /></span>
     </div>
   )
